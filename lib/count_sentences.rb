f12579b40e3e8b513fad4b97b425ad1a6080bea7
Michelle_Mungai <chellymungai@gmail.com>
2023-02-12 19:05:29 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.include?(".")
   end
 
   def question?
+    self.include?("?")
 
   end
 
   def exclamation?
+    self.include?("!")
 
   end
 
   def count_sentences
-
+   self.squeeze("!!").split(/[!.?]/).length
   end
 end
